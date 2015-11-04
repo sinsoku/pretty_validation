5f2a6d3509808ea56cc225c89b81f87512925238
sinsoku <sinsoku.listy@gmail.com>
2015-11-04 09:39:23 +0900

    Lazy load

--- lib/pretty_validation/railtie.rb
+++ lib/pretty_validation/railtie.rb
@@ -6,8 +6,10 @@ module PrettyValidation
       load 'pretty_validation/tasks/validation.rake'
     end
 
-    ActiveSupport.on_load :active_record do
-      ActiveRecord::Base.include PrettyValidation::ValidationFindable
+    initializer 'pretty_validation' do
+      ActiveSupport.on_load :active_record do
+        ActiveRecord::Base.include PrettyValidation::ValidationFindable
+      end
     end
   end
 end
