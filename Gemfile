1f7908e8c47c18ba2a1b09c1b7ed21120879c44b
sinsoku <sinsoku.listy@gmail.com>
2015-12-01 13:21:27 +0900

    codecov in codeclimate out

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in pretty_validation.gemspec
 gemspec
+
+gem 'simplecov', require: false
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,12 +2,16 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require 'rails'
 
+require 'yaml'
+yaml = YAML.load_file('.travis.yml')
+
+require 'simplecov'
+SimpleCov.start
+
 begin
-  require 'yaml'
-  yaml = YAML.load_file('.travis.yml')
   if ENV['TRAVIS_RUBY_VERSION'] == yaml['rvm'][-1].to_s
-    require 'codeclimate-test-reporter'
-    CodeClimate::TestReporter.start
+    require 'codecov'
+    SimpleCov.formatter = SimpleCov::Formatter::Codecov
   end
 rescue LoadError
   # nothing
